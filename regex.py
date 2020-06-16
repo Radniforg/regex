dd08a165308c6e077f00170356fda15a2a4d09bc
Ivan Alexandrov <radniforg@yandex.ru>
2020-06-16 18:35:06 +0300

    Complete script

--- regex.py
+++ regex.py
@@ -6,6 +6,8 @@ def LFS(list_name):
     new_list = []
     if re.search(' ', list_name[0]):
         new_list = re.split(' ', list_name[0])
+        if len(new_list) == 2:
+            new_list.append('')
     else:
         new_list.append(list_name[0])
         if re.search(' ', list_name[1]):
@@ -43,16 +45,35 @@ def phone(number):
         formated_phone = tiles
     return formated_phone
 
+def duplicate_deletion(contact_list):
+    marked = []
+    for contacts in range(1, len(contact_list)):
+        approach = 1
+        while contacts + approach < len(contact_list):
+            if contact_list[contacts][0] == contact_list[contacts + approach][0]:
+                if contact_list[contacts][1] == contact_list[contacts + approach][1]:
+                    marked.append(contacts + approach)
+                    for detail in range(2, len(contact_list[contacts])):
+                        if contact_list[contacts][detail] == '' or  contact_list[contacts][detail] == ' ':
+                            contact_list[contacts][detail] = contact_list[contacts + approach][detail]
+            approach += 1
+    marked.sort(reverse=True)
+    for duplicate in marked:
+        del (contact_list[duplicate])
+    return contact_list
+
 with open('phonebook_raw.csv') as f:
     rows = csv.reader(f, delimiter=',')
     contacts_list = list(rows)
-
-
-#[5] - телефон
+# pprint(contacts_list)
 
 for contacts in contacts_list[1:]:
     new_name_list = LFS(contacts[:3])
     contacts[:3] = new_name_list[:3]
     contacts[5] = phone(contacts[5])
 
-pprint(contacts_list)
+duplicate_deletion(contacts_list)
+
+with open("phonebook.csv", "w") as f:
+  datawriter = csv.writer(f, delimiter=',')
+  datawriter.writerows(contacts_list)
